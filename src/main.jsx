971eba8d0ee0e6b4c733def52fe07ffe94e26e40
Mohammad <myarali1345@gmail.com>
2023-01-04 21:22:37 +0330

    add font and theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { MantineProvider, Button } from "@mantine/core";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider
+      theme={{ colorScheme: 'dark', fontFamily:"Poppins" }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <App />
+    </MantineProvider>
   </React.StrictMode>
-)
+);
