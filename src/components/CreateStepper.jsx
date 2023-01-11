16cea2fa18512bee99c63a917a50b7f5d7ca4a6c
Mohammad <myarali1345@gmail.com>
2023-01-11 11:02:16 +0330

    fix: add loading and disabled button in second step

--- src/components/CreateStepper.jsx
+++ src/components/CreateStepper.jsx
@@ -1,37 +1,51 @@
 import React, { useContext } from "react";
 import { Button, Group, Card, Timeline } from "@mantine/core";
 import { StepperContext } from "../context/StepperContext";
-import { FiZap, FiUser } from "react-icons/fi"
+import { FiZap, FiUser } from "react-icons/fi";
 import StepperChild from "./StepperChild";
 import Instructions from "./Card/Instruction.firstStep";
 import UserForm from "./Card/UserForm.secondStep";
-
+import { getUser } from "../api";
+import { useQuery } from "@tanstack/react-query";
 
 const CreateStepper = () => {
   const { active, nextStep, prevStep } = useContext(StepperContext);
-  
+  const isTrue = active === 1;
+  const { data, isLoading } = useQuery(["user"], getUser, { enabled: isTrue });
   return (
     <>
-      <Card withBorder shadow="md" radius="md" style={{width:"635px"}}>
+      <Card withBorder shadow="md" radius="md" style={{ width: "635px" }}>
         <Timeline active={active} bulletSize={24} lineWidth={2}>
-          <Timeline.Item title="Wizard Service" bullet={ <FiZap size={14} /> }>
-            <StepperChild id={0} title="first step" desc="You can read instruction in this step 📑">
+          <Timeline.Item title="Wizard Service" bullet={<FiZap size={14} />}>
+            <StepperChild
+              id={0}
+              title="first step"
+              desc="You can read instruction in this step 📑"
+            >
               <Instructions />
             </StepperChild>
           </Timeline.Item>
 
-          <Timeline.Item title="Profile" bullet={ <FiUser size={14} /> }>
-            <StepperChild id={1} title="second step" desc="You must enter your identity here 🧬">
+          <Timeline.Item title="Profile" bullet={<FiUser size={14} />}>
+            <StepperChild
+              id={1}
+              title="second step"
+              desc="You must enter your identity here 🧬"
+            >
               <UserForm />
             </StepperChild>
           </Timeline.Item>
 
           <Timeline.Item title="Pull request" lineVariant="dashed">
-            <StepperChild id={2} title="third step">Hi mom</StepperChild>
+            <StepperChild id={2} title="third step">
+              Hi mom
+            </StepperChild>
           </Timeline.Item>
 
           <Timeline.Item title="Code review">
-            <StepperChild id={3} title="third step">Hi mom</StepperChild>
+            <StepperChild id={3} title="third step">
+              Hi mom
+            </StepperChild>
           </Timeline.Item>
         </Timeline>
 
@@ -44,7 +58,13 @@ const CreateStepper = () => {
           >
             Back
           </Button>
-          <Button variant="outline" color="teal" onClick={nextStep}>
+          <Button
+            variant="outline"
+            color="teal"
+            onClick={nextStep}
+            disabled={active === 1 ? (data?.length === 0 ? true : false) : ""}
+            loading={active === 1 ? (isLoading ? true : false) : ""}
+          >
             {active === 3 ? <p>Checkout & Buy</p> : <p>Next step</p>}
           </Button>
         </Group>
