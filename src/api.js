54384c32389e0e4725188010aa88cca46286e190
Mohammad <myarali1345@gmail.com>
2023-02-04 13:11:30 +0330

    fix: merge conflict bug

--- src/api.js
+++ src/api.js
@@ -7,7 +7,7 @@ export const addUser = async (data) => {
 export const getServices = async () => {
   let services = await axios.get("http://localhost:3000/services");
   return services.data;
-
+}
 export const getUser = async () => {
   const user = await axios.get("http://localhost:3000/user");
   return user.data;
